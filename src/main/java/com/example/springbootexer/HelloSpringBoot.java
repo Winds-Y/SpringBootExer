a1452f1d370e930bf536bb6a5fb9b1826d38b433
ycz <2155513297@qq.com>
2018-12-10 16:50:43 +0800

    模拟登陆

--- src/main/java/com/example/springbootexer/HelloSpringBoot.java
+++ src/main/java/com/example/springbootexer/HelloSpringBoot.java
@@ -1,6 +1,8 @@
 package com.example.springbootexer;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,8 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class HelloSpringBoot {
-    @RequestMapping("/index")
-    public String index(){
+    @RequestMapping("/")
+    public String index(ModelMap map){
+        map.addAttribute("host","http://blog.didispace.com");
         return "hello";
     }
 }
---
+++ src/main/java/entity/User.java
@@ -0,0 +1,53 @@
+package entity;
+
+import java.util.Date;
+
+public class User {
+    private String id;
+    private String code;
+    private String mail;
+    private Date time;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+    @Override
+    public boolean equals(Object o){
+        if(o==this) {
+            return true;
+        }
+        if(!(o instanceof User)){
+            return false;
+        }
+        User user=(User)o;
+        return (user.getId().equals(this.id)) &&(user.getCode().equals(this.code))&&(user.getMail().equals(this.mail));
+    }
+}
